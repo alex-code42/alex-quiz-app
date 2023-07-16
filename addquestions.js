e0b452fbfe4bbed4e79a7c66b0811ccb65e70f19
Alex <areichel1986@gmail.com>
2023-07-16 16:07:46 +0200

    adding trigger_animation

--- addquestions.js
+++ addquestions.js
@@ -114,9 +114,23 @@ else{
 answer.style.display = "none"
 
 
-
-
-
 };
+
+const square = document.querySelector('.square');
+square.classList.remove('square-transition');
+
+// Create the observer, same as before:
+const observer = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      square.classList.add('square-transition');
+      return;
+    }
+
+    square.classList.remove('square-transition');
+  });
+});
+
+observer.observe(document.querySelector('.square-wrapper'));
 
 
